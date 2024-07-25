d68a2276122f5b0cdfe23d076f9a5252ca9ea6c5
Al13n0231 <139857884+Al13n0231@users.noreply.github.com>
2024-07-25 17:14:07 +0800

    shortened the overall code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,58 +1,39 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 // 1. compute the averages for the other two directors
 // 2. create a method that finds the director with the highest average
 // 3. END Instead of hard coding the rating, generate random numbers https://www.geeksforgeeks.org/generating-random-numbers-in-java/
 public class Main {
     public static void main(String[] args) {
-        Movie jurassicPark = new Movie("Jurassic Park","steven spielberg", (int)(Math.random()*10+1));
-        Movie readyPlayerOne = new Movie("Ready Player One","steven spielberg", (int)(Math.random()*10+1));
-        Movie lincoln = new Movie("Lincoln","steven spielberg", (int)(Math.random()*10+1));
+        Movie[] movies = {
+                new Movie("Jurassic Park", "steven spielberg", generateRandomRating()),
+                new Movie("Ready Player One", "steven spielberg", generateRandomRating()),
+                new Movie("Lincoln", "steven spielberg", generateRandomRating()),
+                new Movie("The Shining", "stanley kubrick", generateRandomRating()),
+                new Movie("Eyes Wide Shut", "stanley kubrick", generateRandomRating()),
+                new Movie("Full Metal Jacket", "stanley kubrick", generateRandomRating()),
+                new Movie("Psycho", "alfred hitchcock", generateRandomRating()),
+                new Movie("Vertigo", "alfred hitchcock", generateRandomRating()),
+                new Movie("The Birds", "alfred hitchcock", generateRandomRating())
+        };
 
-        Movie theShining = new Movie("The Shining","stanley kubrick", (int)(Math.random()*10+1));
-        Movie eyesWideShut = new Movie("Eyes Wide Shut","stanley kubrick", (int)(Math.random()*10+1));
-        Movie fullMetalJacket = new Movie("Full Metal Jacket","stanley kubrick", (int)(Math.random()*10+1));
+        Map<String, Double> directorRatingSums = new HashMap<>();
+        Map<String, Integer> directorMovieCounts = new HashMap<>();
 
-        Movie psycho = new Movie("Psycho","alfred hitchcock", (int)(Math.random()*10+1));
-        Movie vertigo = new Movie("Vertigo","alfred hitchcock", (int)(Math.random()*10+1));
-        Movie theBirds = new Movie("The Birds","alfred hitchcock", (int)(Math.random()*10+1));
-        ArrayList<Movie> movies = new ArrayList<Movie>();
-        movies.add(jurassicPark);
-        movies.add(readyPlayerOne);
-        movies.add(lincoln);
-        movies.add(theShining);
-        movies.add(eyesWideShut);
-        movies.add(fullMetalJacket);
-        movies.add(psycho);
-        movies.add(vertigo);
-        movies.add(theBirds);
-
-        int counter = 0;
-        HashMap<String, Double> directorRatingAvg = new HashMap<String, Double>();
-        HashMap<String, Double> directorRatingSums = new HashMap<String, Double>();
-        for(Movie movie:movies){
-            if (directorRatingSums.containsKey(movie.getDirector())){
-                Double currentSum = directorRatingSums.get(movie.getDirector());
-                directorRatingSums.put(movie.getDirector(), currentSum + movie.getRating());
-            }
-            else {
-                directorRatingSums.put(movie.getDirector(), (double) movie.getRating());
-            }
-            if (counter == 2){
-               directorRatingAvg.put(movie.getDirector(), directorRatingSums.get(movie.getDirector()) / 3);
-               counter = 0;
-
-            }
-            counter += 1;
-
+        for (Movie movie : movies) {
+            directorRatingSums.merge(movie.getDirector(), (double) movie.getRating(), Double::sum);
+            directorMovieCounts.merge(movie.getDirector(), 1, Integer::sum);
         }
-        directorRatingAvg.forEach((key, value) -> System.out.println(key + " " + value));
 
+        directorRatingSums.forEach((director, sum) -> {
+            double average = sum / directorMovieCounts.get(director);
+            System.out.println(director + " " + average);
+        });
+    }
 
-
+    private static int generateRandomRating() {
+        return new Random().nextInt(10) + 1;
     }
 }
 
